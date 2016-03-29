ee36abc85949f0ba5bf489ebbce5036341e4db56
Dmytro_Adonin <Dmytro_Adonin@epam.com>
2016-03-29 16:10:06 +0300

    Forgot to uncomment one place

--- src/test/java/com/igniteoutsourcing/domino/service/impl/ConsoleInputValidatorTest.java
+++ src/test/java/com/igniteoutsourcing/domino/service/impl/ConsoleInputValidatorTest.java
@@ -18,11 +18,11 @@ public class ConsoleInputValidatorTest {
         String digitNotInRange = "50";
         String digitInRange = "10";
 
-        //boolean nonDigitCheck = testedValidator.isValid(nonDigit);
+        boolean nonDigitCheck = testedValidator.isValid(nonDigit);
         boolean digitNotInRangeCheck = testedValidator.isValid(digitNotInRange);
         boolean digitInRangeCheck = testedValidator.isValid(digitInRange);
 
-        //assertFalse(UNEXPECTED_VALIDATION_RESULT, nonDigitCheck);
+        assertFalse(UNEXPECTED_VALIDATION_RESULT, nonDigitCheck);
         assertFalse(UNEXPECTED_VALIDATION_RESULT, digitNotInRangeCheck);
         assertTrue(UNEXPECTED_VALIDATION_RESULT, digitInRangeCheck);
     }
